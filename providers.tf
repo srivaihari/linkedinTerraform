79e08847601b36c823c781898e3e49f085b8fdd9
Hariharan Lekshmi <hariharanlekshmi@Hariharans-MacBook-Pro.local>
2025-09-17 13:17:43 -0700

    Added new providers for 2 regions

--- providers.tf
+++ providers.tf
@@ -2,13 +2,20 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.31.0"
+      version = ">= 5.31.0"
     }
   }
 }
 
 provider "aws" {
   region = "us-east-1"
-  profile = "Helen"
+  profile = "hari"
   # Configuration options
 }
+
+provider "aws" {
+  alias = "west"
+  region = "us-west-1"
+  profile = "hari"
+}
+
